e83e239116e332ac75e09d40352325f1aff78958
JunJiEung <wjswldmd@gmail.com>
2017-08-06 01:46:15 +0900

    first Commit

---
+++ NORU/Assets/Maps/Map_2/Trap.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Trap : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.position = new Vector3(7, 0, 0);
+    }
+}
