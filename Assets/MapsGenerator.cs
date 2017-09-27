9af5564121897f0e21b805c8d6469be2e59b6171
JunJiEung <wjswldmd@gmail.com>
2017-09-27 21:22:18 +0900

    기본 맵 로직, 방마다 배열 생성 완료

--- Assets/MapsGenerator.cs
+++ Assets/MapsGenerator.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//코드 전체에서 x가 높이 축, y가 너비 축
+
 public class MapsGenerator : MonoBehaviour {
 
     public int room_Count;
     public int width, height;
-    public int[,] map_Info, check;
+    int[,] check;
     List<Vector2> select_Rooms;
-    List<Room> generated_Rooms;
+
+    public List<Room> generated_Rooms;
+    public Room[,] map_Info;
 
     void Start () {
         Init();
@@ -19,10 +24,11 @@ public class MapsGenerator : MonoBehaviour {
     //변수 초기화, (배열, 리스트 초기화)
     void Init()
     {
-        map_Info = new int[height, width];
         check = new int[height, width];
         select_Rooms = new List<Vector2>();
+
         generated_Rooms = new List<Room>();
+        map_Info = new Room[height, width];
     }
 
     //맵 생성 코어 함수
@@ -40,16 +46,16 @@ public class MapsGenerator : MonoBehaviour {
         int random;
         int count = 0;
 
-        while (count < room_Count && 0 < select_Rooms.Count)    //방 개수가 목표치까지 생기거나, 리스트가 빌때까지(경우 없음)
-        {
-            random = Random.Range(0, select_Rooms.Count);   //생성 가능한 방을 뽑고 제거.
-            generated_Rooms.Add(new Room(select_Rooms[random][0], select_Rooms[random][1]));      //생성된 방은 다른 리스트에 저장
+        while (count < room_Count && 0 < select_Rooms.Count)  //방 개수가 목표치까지 생기거나, 리스트가 빌때까지(경우 없음)
+        {   
+            random = Random.Range(0, select_Rooms.Count);  //생성 가능한 방을 뽑고 제거.
             temp_Y = (int)select_Rooms[random][0];
             temp_X = (int)select_Rooms[random][1];
+
+            generated_Rooms.Add(new Room(temp_Y, temp_X));  //생성된 방은 다른 리스트에 저장
+            map_Info[temp_Y, temp_X] = generated_Rooms[generated_Rooms.Count - 1];  //찾기 쉽게 이차원 배열에 등록
+
             select_Rooms.RemoveAt(random);
-
-            map_Info[temp_Y, temp_X] = (count + 1);
-
             if (temp_Y + 1 < height && check[temp_Y + 1, temp_X] != 1)
             {
                 select_Rooms.Add(new Vector2(temp_Y + 1, temp_X));
@@ -76,15 +82,59 @@ public class MapsGenerator : MonoBehaviour {
         return true;
     }
 }
-
-class Room
+public class Room
 {
-    private float x;  //height
-    private float y;  //width
+    private int x;  //x가 높이 축 전체 맵에서
+    private int y;  //y가 너비 축 전체 맵에서
 
-    public Room(float v1, float v2)
+    private int width;
+    private int height;
+
+    private int[,] cell_Info;
+
+    private Room()
     {
-        this.x = v1;
-        this.y = v2;
+        width = 16;
+        height = 10;
+    }
+
+    public Room(int v1, int v2) : this()
+    {
+        SetRoomPos(v1, v2);
+        SetRoomSize(0);
+    }
+
+    public Room(int v1, int v2, int v3) : this()
+    {
+        SetRoomPos(v1, v2);
+        SetRoomSize(v3);
+    }
+
+    void SetRoomPos(int v1, int v2)
+    {
+        x = v1;
+        y = v2;
+    }
+
+    void SetRoomSize(int size)
+    {
+        float w = 1.6f;
+        float h = 1.0f;
+
+        int minWidth = width - (int)(w * size);
+        int maxWidth = width + (int)(w * size);
+
+        int minHeight = height - (int)(h * size);
+        int maxHeight = height + (int)(h * size);
+
+        width = Random.Range(minWidth, maxWidth);
+        height = Random.Range(minHeight, maxHeight);
+
+        if (width < 8)
+            width = 8;
+        if (height < 5)
+            height = 5;
+
+        cell_Info = new int[height, width];
     }
 }
